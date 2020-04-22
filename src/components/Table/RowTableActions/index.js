343ab9e7a6f3a856b32fc28ec3e7eda9e408a0c9
smit patel <patelsmit110799@gmail.com>
2020-04-22 23:07:50 +0530

    add-row feature removed

--- src/components/Table/RowTableActions/index.js
+++ src/components/Table/RowTableActions/index.js
@@ -3,12 +3,9 @@ import { Tooltip, Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 function RowTableActions(props) {
-    const title = props.isFromColumn ? "Click To Add A New Column" : "Click to add a block below";
     return (
         <div>
-            <Tooltip title={title} placement="bottom">
-                <Button type="default" icon={<PlusOutlined />} onClick={props.handleAdd} />
-            </Tooltip>
+            hello
         </div>
     )
 }
